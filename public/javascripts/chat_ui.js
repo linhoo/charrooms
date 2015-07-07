786755ca3a0fda5abdb8981976ca61c1df178508
terry <linhu.li2012@hotmail.com>
2015-07-07 21:50:01 +0800

    Fix the issue that the room cann't changed when click on thee room lisst items

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
         }
 
         $("#room-list div").click(function () {
-            chatApp.processCommand("/join" + $(this.text));
+            chatApp.processCommand("/join" + $(this).text());
             $("#send-message").focus();
         });
     });
